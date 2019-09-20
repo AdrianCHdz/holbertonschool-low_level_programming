d428021959aa0e3ff55099ddfb1e0c9ea56c64ca
AdnxCHdz <1214@holbertonschool.com>
2019-09-19 23:43:41 -0500

    Printbase16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - printbase16
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char num;
+	char alpha;
+
+	for (num = '0'; num <= '9'; num++)
+		putchar(num);
+	for (alpha = 'a'; alpha <= 'f'; alpha++)
+		putchar(alpha);
+	putchar('\n');
+	return (0);
+}
