cff9ded0368cb606bb335e0f6ae0818349eea8e0
AdnxCHdz <1214@holbertonschool.com>
2019-09-19 23:42:32 -0500

    CBA|ABC

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - tebahpla
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char teb;
+
+	for (teb = 'z'; teb >= 'a'; teb--)
+		putchar(teb);
+	putchar('\n');
+	return (0);
+}
