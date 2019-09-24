4323044f282a3a0393c51f560bf4da7e50892e45
AdnxCHdz <1214@holbertonschool.com>
2019-09-24 17:58:39 -0500

    folder

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -1,5 +1,9 @@
 #ifndef POSITIVE_OR_NEGATIVE_H
 #define POSITIVE_OR_NEGATIVE_H
 void positive_or_negative(int i);
-int largest_number(int, int, int);
+#endif
+
+#ifndef LARGEST_NUMBER_H
+#define LARGEST_NUMBER_H
+int largest_number(int a, int b, int c);
 #endif
