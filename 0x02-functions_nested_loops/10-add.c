e02a6cceec118f69861002adb3f53b53125ff50b
AdnxCHdz <1214@holbertonschool.com>
2019-09-24 14:15:37 -0500

    Sum

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,7 +1,8 @@
 #include "holberton.h"
 /**
  * main - check the code for Holberton School students.
- *
+ *@n: Value to be summed with m
+ *@m: Value that will be summed with n
  * Return: Always 0.
  */
 int add(int n, int m)
