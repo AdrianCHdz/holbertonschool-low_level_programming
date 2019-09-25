ffd8496f60a714243d569b5e3265595fcbcbf099
AdnxCHdz <1214@holbertonschool.com>
2019-09-25 14:24:31 -0500

    addition

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - sum of multibples of 3 and 5
+ *
+ * Return: Always 0 if succesful
+ */
+int main(void)
+{
+	long int a;
+	long int mult;
+	long int b;
+
+	mult = 0;
+	for (a = 0; a <= 1024; a++)
+	{
+		if ((a % 5 == 0 || a % 3 == 0) && a < 1024)
+		{
+			mult = mult + a;
+		}
+	}
+	printf("%ld", mult);
+	putchar('\n');
+}
