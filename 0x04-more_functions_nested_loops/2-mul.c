5fef813800e32fb834385fb397ffbb3bb1606deb
AdnxCHdz <1214@holbertonschool.com>
2019-09-26 18:17:29 -0500

    multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * mul - Multiplication.
+ *@a: var to be multiplied
+ *@b: var to be multiplied
+ * Return: the value of multiplicatioin
+ */
+int mul(int a, int b)
+{
+	int c = a * b;
+
+	return (c);
+}
