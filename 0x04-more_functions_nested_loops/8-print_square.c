8209ce0967987ca5322183410f14fa91d4a0d9c4
AdnxCHdz <1214@holbertonschool.com>
2019-09-26 18:20:28 -0500

    print squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * print_square - check the code for Holberton School students.
+ *@size: the size of the square
+ * Return: Always 0.
+ */
+void print_square(int size)
+{
+	int a;
+	int b;
+
+	if (size > 0)
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (b = 0; b < size; b++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
