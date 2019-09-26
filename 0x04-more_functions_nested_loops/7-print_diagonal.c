8fbd09436b520cd99dc89b2d06ae46baf24de754
AdnxCHdz <1214@holbertonschool.com>
2019-09-26 18:19:40 -0500

    print a diagonal line

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - check the code for Holberton School students.
+ * @n: the value that will determine the number of diagonals
+ * Return: Always 0.
+ */
+void print_diagonal(int n)
+{
+	int a;
+	int b;
+
+	if (n > 0)
+	{
+		for (a = 0; a < n; a++)
+		{
+			for (b = 0; b < a; b++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
