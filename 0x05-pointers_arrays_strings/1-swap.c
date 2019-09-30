f23c11783f9a7ffcbff1a558a8113b03dc601d27
AdnxCHdz <1214@holbertonschool.com>
2019-09-30 17:27:50 -0500

    Swap values of the variables

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * swap_int - change both numeric values
+ * @a: the fisrt value to be changed
+ * @b: the second value to be changed
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
