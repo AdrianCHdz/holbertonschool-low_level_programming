7bbb763dbb504b47b2937c1ee143e821ae31fbe9
AdnxCHdz <1214@holbertonschool.com>
2019-09-30 17:24:41 -0500

    Change value to 98"

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ * reset_to_98 - will reset the value of n to 98.
+ * @n: pointer from which the values will be taken.
+ * Return: Always 0.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
