80581879d4ee8737a20e341a6ccebdd9a88c70d3
AdnxCHdz <1214@holbertonschool.com>
2019-10-01 17:47:52 -0500

    Rev string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,7 +7,7 @@
 void rev_string(char *s)
 {
 	int position = 0;
-	int b = 0;
+	int backwards = 0;
 	int c;
 
 	while (s[position] != '\0')
@@ -15,12 +15,10 @@ void rev_string(char *s)
 		position++;
 	}
 	position -= 1;
-	while (s[position] > s[b])
+	while (position > backwards)
 	{
 		c = s[position];
-		s[position] = s[b];
-		s[b] = c;
-		b++;
-		position--;
+		s[position--] = s[backwards];
+		s[backwards++] = c;
 	}
 }
