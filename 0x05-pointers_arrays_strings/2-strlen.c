6f1e18dc5f70693ecba02a3837adb4fe71dfc7fb
AdnxCHdz <1214@holbertonschool.com>
2019-10-01 09:17:20 -0500

    Strlen function

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	int position;
+
+	while (position[s] != '\0')
+	{
+		position++;
+	}
+	return (position);
+}
