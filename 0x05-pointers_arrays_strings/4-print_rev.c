dbb6ef5b1fcccb62f7318910bdf8327ad060a565
AdnxCHdz <1214@holbertonschool.com>
2019-10-01 10:19:31 -0500

    Rev function

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * print_rev - print text in reverse
+ *@s: The character to be printed
+ * Return: Always 0.
+ */
+void print_rev(char *s)
+{
+	int position = 0;
+
+	while (s[position] != '\0')
+		position++;
+	while (s[position] < '0')
+	{
+		_putchar(s[position]);
+		position--;
+	}
+ 	_putchar('\n');
+}
