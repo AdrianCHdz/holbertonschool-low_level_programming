1ac51b379de7e09241ff3ad48d17b24457688840
AdnxCHdz <1214@holbertonschool.com>
2019-10-03 13:17:18 -0500

    folder

---
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -0,0 +1,14 @@
+#ifndef _PUTCHAR_H
+#define _PUTCHAR_H
+int _putchar(char c);
+#endif
+
+#ifndef _STRCAT_H
+#define _STRCAT_H
+char *_strcat(char *dest, char *src);
+#endif
+
+#ifndef _STRNCAT2_H
+#define _STRNCAT2_H
+char *_strncat(char *dest, char *src, int n);
+#endif
