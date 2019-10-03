8921624585d81eede4ddd0a8d8c8f4d244bd6734
AdnxCHdz <1214@holbertonschool.com>
2019-10-03 13:15:42 -0500

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * _strcat - adds a new string to the principal string
+ * @dest: the string from which is going to be added the next.
+ * @src: the new string that will be added.
+ * Return: Always 0.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int a = 0;
+	int b = 0;
+
+	while (dest[a] != '\0')
+	{
+		a++;
+	}
+	while (src[b] != '\0')
+	{
+		dest[a] = src[b];
+		b++;
+		a++;
+		dest[a+1] = '\0'
+	}
+	return (dest);
+}
