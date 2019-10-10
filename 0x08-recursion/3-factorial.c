8ceb62da268f9107dcd44397d33a19a12831e043
AdnxCHdz <1214@holbertonschool.com>
2019-10-09 23:25:57 -0500

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * factorial - prints the factorial of the number.
+ *
+ * @n: the value from which the factorial will be found.
+ * Return: Always the factorial number
+ * Return: -1 if the number is negative
+ * 1 if the number is 0
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+}
