3c2fdd886c6474b690fbf711fd5e69fdc5e1b477
AdnxCHdz <1214@holbertonschool.com>
2019-10-09 23:09:25 -0500

    Function folder

---
+++ 0x08-recursion/holberton.h
@@ -0,0 +1,39 @@
+#ifndef _PUTS_RECURSION_H
+#define _PUTS_RECURSION_H
+void _puts_recursion(char *s);
+#endif
+
+#ifndef _PUTCHAR_H
+#define _PUTCHAR_H
+int _putchar(char c);
+#endif
+
+#ifndef _PRINT_REV_RECURSION_H
+#define _PRINT_REV_RECURSION_H
+void _print_rev_recursion(char *s);
+#endif
+
+#ifndef _STRLEN_RECURSION_H
+#define _STRLEN_RECURSION_H
+int _strlen_recursion(char *s);
+#endif
+
+#ifndef FACTORIAL_H
+#define FACTORIAL_H
+int factorial(int n);
+#endif
+
+#ifndef POW_RECURSION_H
+#define POW_RECURSION_H
+int _pow_recursion(int x, int y);
+#endif
+
+#ifndef _SQRT_RECURSION_H
+#define _SQRT_RECURSION_H
+int _sqrt_recursion(int n);
+#endif
+
+#ifndef IS_PRIME_NUMBER_H
+#define IS_PRIME_NUMBER_H
+int is_prime_number(int n);
+#endif
