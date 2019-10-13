790beef54ae634f77fe39217238a5125bf75cc40
AdnxCHdz <1214@holbertonschool.com>
2019-10-13 17:45:50 -0500

    print arguments recieved

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - Use the arguments that will be passed in the program
+ * @argc: counter
+ * @argv: the vector of the argument
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int a;
+
+	for(a = 1; a < argc; a++)
+	{
+		printf("%s\n", argv[a]);
+	}
+	return (0);
+}
