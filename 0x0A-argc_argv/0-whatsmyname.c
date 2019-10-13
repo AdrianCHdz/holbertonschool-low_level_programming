239e4ba100409c2dba41181ed7070f4f1468940d
AdnxCHdz <1214@holbertonschool.com>
2019-10-13 13:54:56 -0500

    name of the program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - Use the arguments that will be passed in the program
+ * @argc: counter
+ * @argv: the vector of the argument
+ * REturn: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
