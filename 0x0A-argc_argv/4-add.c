3aee4a0d62cb962f41458729b82831abf9100885
AdnxCHdz <1214@holbertonschool.com>
2019-10-13 19:51:45 -0500

    addition

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,37 +1,29 @@
+#include "holberton.h"
 #include <stdio.h>
-#include <stdlib.h> /** atoi function */
+#include <stdlib.h>
 #include <ctype.h>
 /**
  * main - Use the arguments that will be passed in the program
  * @argc: counter
  * @argv: the vector of the argument
  * Return: Always 0 if the vector is the name of the program.
- * or if no number is passed to the program.
- * 1 if the argument passed to the program is a letter.
  */
 int main(int argc, char *argv[])
 {
 	int a, b, add = 0;
 
-	if (argc > 0)
+	for (a = 1; a < argc; a++)
 	{
-		for (a = 1; a < argc; a++)
+		for (b = 0; argv[a][b] != '\0'; b++)
 		{
-			for (b = 0; argv[a][b] != '\0'; b++)
+			if (!(isdigit(argv[a][b])))
 			{
-				if (!(isdigit(argv[a][b])))
-				{
-					printf("Error\n");
-					return (1);
-				}
+				printf("Error\n");
+				return (1);
 			}
-			add += atoi(argv[a]);
 		}
-		printf("%d\n", add);
+		add += atoi(argv[a]);
 	}
-	else
-	{
-		printf("0");
-		return (0);
-	}
+	printf("%d\n", add);
+	return (0);
 }
