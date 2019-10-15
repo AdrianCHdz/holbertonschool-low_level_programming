384e09f98c7aaec1d31789741e24e0d30e54b51b
AdnxCHdz <1214@holbertonschool.com>
2019-10-15 16:17:34 -0500

    Create an array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ *
+ *
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int a;
+	char *string;
+
+	string = malloc(sizeof(char) * size);
+	if (size == 0)
+	{
+		return ('\0');
+	}
+	else if (string == 0)
+	{
+		return ('\0');
+	}
+	for (a = 0; a < size; a++)
+	{
+		string[a] = c;
+	}
+	return (string);
+}
