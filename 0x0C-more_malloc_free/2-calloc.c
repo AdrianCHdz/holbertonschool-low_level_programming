62961bf395ba0a9064b84a191095f44fcb66b5c1
AdnxCHdz <1214@holbertonschool.com>
2019-10-17 16:15:08 -0500

    calloc recreation

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * _calloc - prints buffer in hexa
+ * Return: dimensions of the string
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *isize;
+	unsigned int a = 0;
+	unsigned int mul = nmemb * size;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	isize = (void *)malloc(mul);
+	if (isize == NULL)
+	{
+		return (NULL);
+	}
+	while (a < mul)
+	{
+		*(isize + a) = 0;
+		a++;
+	}
+	return (isize);
+}
