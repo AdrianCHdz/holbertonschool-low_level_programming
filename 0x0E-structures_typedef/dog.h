b0ad2d380088d78f341d9f668397ee29862fa232
AdnxCHdz <1214@holbertonschool.com>
2019-10-21 14:41:15 -0500

    Folder

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+/**
+ * struct dog - the description of the doggo
+ * @name: The name of the puppy
+ * @age: How old is the pup
+ * @owner: The owner of the dude.
+ */
+#ifndef DOG_H
+#define DOG_H
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+#endif
