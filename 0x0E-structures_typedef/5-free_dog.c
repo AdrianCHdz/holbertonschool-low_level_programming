f2a36d3aae8bf28d487c946bfbb23bc6fe19c5c7
AdnxCHdz <1214@holbertonschool.com>
2019-10-21 20:43:29 -0500

    free dog

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -2,8 +2,8 @@
 #include "dog.h"
 
 /**
- * main - check the code for Holberton School students.
- *
+ * free_dog - frees the dog
+ * @d: the pointer to the struct
  * Return: Always 0.
  */
 void free_dog(dog_t *d)
