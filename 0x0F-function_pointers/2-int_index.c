8b1dcb344d86be1b7e33441c4695beaf4fc6713f
AdnxCHdz <1214@holbertonschool.com>
2019-10-23 15:56:43 -0500

    .gitignore

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - check if a number is equal to 98
+ * @array: the integer to check
+ * @cmp: the pointer to a function.
+ * @size: the size that the array will have
+ *
+ * Return: the number of coincidences of the array.
+ * if the size of the array equals 0 return -1.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int a;
+
+	if (size != 0)
+	{
+		if (array != NULL && cmp != NULL)
+		{
+			for (a = 0; a < size; a++)
+			{
+				if (cmp(array[a]) != 0)
+					return (a);
+			}
+		}
+	}
+	return (-1);
+}
