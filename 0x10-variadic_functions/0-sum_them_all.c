d4a5e23d778a419eef7c73e4c1760afa8ec84768
AdnxCHdz <1214@holbertonschool.com>
2019-10-26 12:28:43 -0500

    [Fix]: Description of functions and parameters.

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,5 +1,11 @@
 #include <stdarg.h>
 #include "variadic_functions.h"
+/**
+ * sum_them_all - sum all the undeterminated parametes
+ * @n: What will define how many numbers will be sumed
+ *
+ * Return: the result of the total sum
+ */
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list add;
