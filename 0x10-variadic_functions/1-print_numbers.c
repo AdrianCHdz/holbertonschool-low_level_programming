6bfdd0c1d24cfc312c156daf2d90c5cce1d495b4
AdnxCHdz <1214@holbertonschool.com>
2019-10-26 12:22:58 -0500

    [Feature]: Adding file that prints indefinite numbers. 	   New file: 1-print_numbers.c

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - prints the numbers and separates them
+ * @separator: what will be separating the numbers.
+ * @n: The variable determining the limit of numbers printed.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = 1;
+	va_list print;
+
+	va_start(print, n);
+	if (n != 0)
+	{
+		while (i <= n)
+		{
+			printf("%d", va_arg(print, int));
+			if (i != n && separator != NULL)
+			{
+				printf("%s", separator);
+			}
+			else if (n == 1)
+			{
+				printf("\n");
+			}
+			i++;
+		}
+		printf("\n");
+		va_end(print);
+	}
+	else
+		return;
+}
