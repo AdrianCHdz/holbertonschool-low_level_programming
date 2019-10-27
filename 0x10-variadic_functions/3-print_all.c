de29590a2fb504756f0efe46eea81792b0671808
AdnxCHdz <1214@holbertonschool.com>
2019-10-26 20:19:33 -0500

    [Feature]: Adding new file which is a small version of printf. 	   new file: 3-print_all.c

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,57 @@
+#include "variadic_functions.h"
+void print_all(const char * const format, ...)
+{
+	void (*p)();
+	char *sep;
+	va_list all;
+	get_f fmt[] = {
+		{"c", frmatchar},
+		{"i", frmatint},
+		{"s", frmatstr},
+		{NULL, NULL}
+	};
+	int i = 0;
+	int j;
+
+	va_start(all, format);
+	sep = "";
+	while (format[i] != '\0')
+	{
+		j = 0;
+		while (j < 3)
+		{
+			if (*fmt[j].frmt == format[i])
+			{
+				printf("%s", sep);
+				p = fmt[j].fs;
+				p(all);
+				sep = ", ";
+			}
+			j++;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(all);
+}
+void frmatchar(va_list all)
+{
+	printf("%c", va_arg(all, int));
+}
+void frmatint(va_list all)
+{
+	printf("%d", va_arg(all, int));
+}
+void frmatstr(va_list all)
+{
+	char *string;
+
+	string = va_arg(all, char *);
+
+	if (string == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", string);
+}
