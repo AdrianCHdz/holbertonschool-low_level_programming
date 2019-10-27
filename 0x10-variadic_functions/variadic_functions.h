57c9d4b16d502f016385ff5a2bbabecb540d16e6
AdnxCHdz <1214@holbertonschool.com>
2019-10-26 20:46:56 -0500

    [Fix]: Ereasing redundant pointer to function call

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,17 +3,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdarg.h>
+typedef struct frmtsp
+{
+	char *frmt;
+	void (*fs)(va_list all);
+} get_f;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-typedef struct frmtsp
-{
-	char *frmt;
-	void (*fs)();
-} get_f;
-void frmatchar();
-void frmatint();
-void frmatstr();
-void frmatflt();
+
 #endif
