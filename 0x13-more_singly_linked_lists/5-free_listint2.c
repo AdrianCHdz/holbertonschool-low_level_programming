d20fe83c90b0796b159dce68daf3848862a383b7
AdnxCHdz <1214@holbertonschool.com>
2019-11-05 21:55:09 -0500

    Correction of double pointer argument given

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,11 +1,14 @@
 #include "lists.h"
 void free_listint2(listint_t **head)
 {
+	listint_t *new;
+
 	if (head == NULL)
 		return;
-	if (head != NULL)
+	while (*head != NULL)
 	{
-		free_listint(head->next);
-		free(head);
+		new = (*head)->next;
+		free(*head);
+		*head = new;
 	}
 }
