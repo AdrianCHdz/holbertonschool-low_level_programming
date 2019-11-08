e8d59eb7e2f09019bba9787fdd0c63e0cddce508
AdnxCHdz <1214@holbertonschool.com>
2019-11-07 21:19:26 -0500

    [Feature]: Adding file that converts decimal to binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_binary - converts a decimal into binary
+ * @n: The number to convert
+ */
+void print_binary(unsigned long int n)
+{
+	if (n != 1 &&  n != 0)
+		print_binary(n >> 1);
+	putchar(n + '0');
+}
