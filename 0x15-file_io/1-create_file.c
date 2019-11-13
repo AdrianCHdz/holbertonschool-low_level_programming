44c00782214b5192d343874e659d4a6cd5e88e4e
AdnxCHdz <1214@holbertonschool.com>
2019-11-13 10:34:29 -0500

    [Feature]: Adding comments to the file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 #include "holberton.h"
+/**
+ * _strlen - finds the lenght of the string
+ * @string: The string to be valued.
+ * Return: The lenght of the string.
+ */
 int _strlen(char *string)
 {
-	return((*string != '\0') ? 1 + _strlen(string + 1) : 0);
+	return ((*string != '\0') ? 1 + _strlen(string + 1) : 0);
 }
+/**
+ * create_file - writes the content of a file
+ * @filename: The name of the file that will be created
+ * @text_content: The content of the text created
+ * Return: 1 in success
+ */
 int create_file(const char *filename, char *text_content)
 {
 	int wr_file = 0;
