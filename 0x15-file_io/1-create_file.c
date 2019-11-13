c482f32e3dfd4a4f91d2d16e781c0e5fc3448ca1
AdnxCHdz <1214@holbertonschool.com>
2019-11-13 10:22:40 -0500

    [Fix]: Where the content is redirected

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
 #include "holberton.h"
+int _strlen(char *string)
+{
+	return((*string != '\0') ? 1 + _strlen(string + 1) : 0);
+}
 int create_file(const char *filename, char *text_content)
 {
 	int wr_file = 0;
 	int a = 0;
-	int fg;
+	int fd;
 
 	if (filename == NULL)
 		return (-1);
-	fg = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	if (fg == -1)
+	if (fd == -1)
 		return (-1);
-	while (text_content)
-	{
-		text_content++;
-		a++;
-	}
-	wr_file = write(fg, text_content, a);
+	a = _strlen(text_content);
+	wr_file = write(fd, text_content, a);
 	if (wr_file == -1)
 	{
 		return (-1);
 	}
-	close(fg);
+	close(fd);
 	return (1);
 }
