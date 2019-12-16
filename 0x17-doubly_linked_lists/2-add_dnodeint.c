1ee2b63db186055442edacdfc0dd335830556f63
AdnxCHdz <1214@holbertonschool.com>
2019-12-15 23:28:56 -0500

    [Fix]: Correction order of setting values

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -14,15 +14,15 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 	newnode->n = n;
 	newnode->prev = NULL;
-	newnode->next = *head;
 	if (head == NULL)
 	{
 		newnode->next = NULL;
 	}
 	else
 	{
-		(*head)->prev = newnode;
+		newnode->next = *head;
 		*head = newnode;
+		(*head)->prev = newnode;
 	}
 	return (newnode);
 }
