9b98c4cdab9cd1540b0327c35cb5e41be320df9f
AdnxCHdz <1214@holbertonschool.com>
2019-12-15 21:50:32 -0500

    [Feature]: Adding file that prints double linked list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * print_dlistint - prints a double linked list
+ * @h: A pointer to the adress of the head
+ * Return: The size of the list
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t n = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		n++;
+	}
+	return(n);
+}
