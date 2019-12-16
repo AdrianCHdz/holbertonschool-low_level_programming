88f067cb690c73bdd470845af52f7254855b6a3b
AdnxCHdz <1214@holbertonschool.com>
2019-12-15 23:45:10 -0500

    [Feature]: Adding file that appends a node at the end

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+/**
+ * add_nodeint_end - Adds a new node in the ned of the list
+ * @head: A double pointer to the list
+ * @n: The element that will take the list
+ * Return: The head with the new node
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *add, *liscounter;
+
+	add = malloc(sizeof(dlistint_t));
+	if (add == NULL)
+		return (NULL);
+	add->n = n;
+	add->prev = NULL;
+	if (*head == NULL)
+	{
+		add->next = *head;
+		*head = add;
+	}
+	else
+	{
+		liscounter = *head;
+		while (liscounter->next != NULL)
+		{
+			liscounter = liscounter->next;
+		}
+		add->prev = liscounter;
+		liscounter->next = add;
+	}
+	return (*head);
+}
