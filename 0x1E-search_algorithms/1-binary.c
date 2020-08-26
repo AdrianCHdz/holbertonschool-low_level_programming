21e32fb51531f2c3e328e1d8fb00d2f5dcbfd0c0
AdrianCHdz <1214@holbertonschool.com>
2020-08-26 01:44:50 +0000

    [Upload]: File that finds the position of value in an array with binary search

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,33 @@
+#include "search_algos.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * binary_search - This funcion talks by itself
+ * @array: an array of integers with undifined numbers
+ * @size: The size of the array
+ * @value: The number to be searched
+ *
+ * Return: index where the value is located
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t min = 0, max = size - 1, tmp = 0;
+
+	if (array)
+	{
+		while (min <= max)
+		{
+			for (tmp = min; tmp <= max; tmp++)
+				printf(tmp != max ? "%d, " : "%d\n",
+				       array[tmp]);
+			tmp = (max + min) / 2;
+			if (array[tmp] > value)
+				max = tmp - 1;
+			else if (array[tmp] < value)
+				min = tmp + 1;
+			else
+				return (tmp);
+		}
+	}
+	return (-1);
+}
