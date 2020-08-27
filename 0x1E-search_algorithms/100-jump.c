b2cc6e5f48c3a8a272561a269160bc562f578bac
AdrianCHdz <1214@holbertonschool.com>
2020-08-27 18:30:35 +0000

    [Feature]: Uploading outdated version of jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -52,7 +52,7 @@ int jump_search(int *array, size_t size, int value)
 		}
 		max = size - 1;
 		printf(found, min, max);
-		res = binary(array, max, value, min);
+		res = customl_search(array, max, value, min);
 		if (res > 0)
 			return (res);
 	}
